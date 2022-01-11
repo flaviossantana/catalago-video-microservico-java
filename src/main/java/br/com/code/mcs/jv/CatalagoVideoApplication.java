c1c32a15f91d47801fadbb746e124533ea54b6b6
flavio.santana <9090flavio@gmail.com>
2022-01-10 21:46:05 -0300

    PONTAPÉ INICIAL - CONFIGURANDO DOCKER

---
+++ src/main/java/br/com/code/mcs/jv/CatalagoVideoApplication.java
@@ -0,0 +1,14 @@
+package br.com.code.mcs.jv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalagoVideoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Olá MUNDO!");
+		SpringApplication.run(CatalagoVideoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/code/mcs/jv/CatalagoVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.code.mcs.jv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalagoVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
